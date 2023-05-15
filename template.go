d6d6b435a1aba636872f0af594ab0aa9564d8c6b
Reales Myles <rclarence123@gmail.com>
2023-05-14 22:24:50 -0500

    Primer commit templates golang

---
+++ template.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"text/template"
+)
+
+// Handler
+func Index(rw http.ResponseWriter, r *http.Request) {
+	// fmt.Fprintf(rw, "Hola mundo")
+	template, err := template.ParseFiles("index.html")
+
+	if err != nil {
+		panic(err)
+	} else {
+		template.Execute(rw, nil)
+	}
+
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", Index)
+
+	//servidor
+	server := &http.Server{
+		Addr:    "localhost:3000",
+		Handler: mux,
+	}
+	fmt.Println("El servidor esta coorriendo en el puerto 3000")
+	fmt.Printf("Run Server: http://localhost:3000/")
+	log.Fatal(server.ListenAndServe())
+}
